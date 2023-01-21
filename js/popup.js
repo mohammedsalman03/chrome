03dc4327a53a37e2a536b754af37ca0ab9e9e899
mohammedsalman03 <mohammedsalmanali03@gmail.com>
2023-01-21 14:03:35 +0000

    changes are almost done

--- js/popup.js
+++ js/popup.js
@@ -2,7 +2,7 @@ async function fetchData() {
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': 'dc8994b59cmsh4a2a7f54d5cb30bp1e20c5jsne08aca9d8b5e',
+            'X-RapidAPI-Key': 'a425bb3a1emshacddd96f1956a77p1d5f37jsnc2a11ed8f617',
             'X-RapidAPI-Host': 'livescore6.p.rapidapi.com'
         }
     };
@@ -12,11 +12,12 @@ async function fetchData() {
 
     console.log('record', record)
     let date= new Date();
+    
     const indices = [0, 1, 2, 93, 94];
 const filteredStages = record.Stages.filter((stage, index) => indices.includes(index));
 document.getElementById("fixtures").innerHTML = filteredStages.map(item => 
     
-`<li>${item.Cnm} | ${item.Snm} | ${item.Events[0].T1[0].Nm} 🆚 ${item.Events[0].T2[0].Nm} |📆 ${date.getDate(item.Events[0].Esd)}/${(date.getMonth(item.Events[0].Esd)+1).toString().padStart(2, '0')}/${date.getFullYear(item.Events[0].Esd)} | ${String(item.Events[0].Esd).slice(8,10)}:${String(item.Events[0].Esd).slice(10,12)} PM</li>`
+`<li>${item.Cnm} | ${item.Snm} | ${item.Events[0].T1[0].Nm} 🆚 ${item.Events[0].T2[0].Nm} | <i class="material-icons" style="font-size:24px">today</i> ${date.getDate(item.Events[0].Esd)}/${(date.getMonth(item.Events[0].Esd)+1).toString().padStart(2, '0')}/${date.getFullYear(item.Events[0].Esd)} | <i class="material-icons" style="font-size:24px">access_alarm</i> ${String(item.Events[0].Esd).slice(8,10)}:${String(item.Events[0].Esd).slice(10,12)} PM</li>`
 ).join(' ');
 
 
